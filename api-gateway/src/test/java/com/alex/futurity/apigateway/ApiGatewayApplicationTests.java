c57a21fb7ceb0919dbb70c78216a11d507622574
ThePersonThat <gaudabrus@inbox.ru>
2021-11-20 19:15:09 +0200

    Initial commit. Generated the api-gateway project

---
+++ api-gateway/src/test/java/com/alex/futurity/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.futurity.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
