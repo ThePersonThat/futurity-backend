f23435302c0461c0ee0333d2ba16aa3777b8eea9
Odeyalo <theprogrammerinfuture@gmail.com>
2022-04-13 21:18:52 +0300

    Added logs to endpoints

--- project-server/src/main/java/com/alex/futurity/projectserver/controller/ColumnController.java
+++ project-server/src/main/java/com/alex/futurity/projectserver/controller/ColumnController.java
@@ -3,6 +3,7 @@ package com.alex.futurity.projectserver.controller;
 import com.alex.futurity.projectserver.dto.*;
 import com.alex.futurity.projectserver.service.ColumnManagerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,12 +14,14 @@ import javax.validation.Valid;
 @Validated
 @RestController
 @AllArgsConstructor
+@Log4j2
 public class ColumnController {
     private final ColumnManagerService columnService;
 
     @PostMapping("/{userId}/column/{projectId}/create")
     public ResponseEntity<IdResponse> createColumn(@PathVariable long userId, @PathVariable long projectId,
                                                    @Valid @RequestBody CreationColumnRequestDTO request) {
+        log.info("Handling new create column request. User id: {}, project id: {}, dto: {}", userId, projectId, request);
         request.setUserId(userId);
         request.setProjectId(projectId);
 
@@ -27,6 +30,7 @@ public class ColumnController {
 
     @GetMapping("/{userId}/column/{projectId}")
     public ResponseEntity<ListResponse<ColumnDTO>> getColumns(@PathVariable long userId, @PathVariable long projectId) {
+        log.info("Handling get columns request. User id: {}, project id: {}", userId, projectId);
         TwoIdRequestDTO request = new TwoIdRequestDTO(userId, projectId);
 
         return ResponseEntity.ok(columnService.getColumns(request));
@@ -35,6 +39,7 @@ public class ColumnController {
     @DeleteMapping("/{userId}/column/{projectId}/delete/{columnId}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteColumn(@PathVariable long userId, @PathVariable long projectId, @PathVariable long columnId) {
+        log.info("Handling delete column request. User id: {}, project id: {}, column id: {}", userId, projectId, columnId);
         ThreeIdRequestDTO request = new ThreeIdRequestDTO(userId, projectId, columnId);
 
         columnService.deleteColumn(request);
@@ -43,6 +48,8 @@ public class ColumnController {
     @PatchMapping("/{userId}/column/{projectId}/index/change")
     public void changeIndexColumn(@PathVariable long userId, @PathVariable long projectId,
                                   @Valid @RequestBody ChangeIndexColumnRequestDTO request) {
+        log.info("Handling change  column index request. User id: {}, project id: {}, dto: {}", userId, projectId, request);
+
         request.setUserId(userId);
         request.setProjectId(projectId);
 
