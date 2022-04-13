d27227f6c009598f09bb63fe8d8280d269f476d1
Odeyalo <theprogrammerinfuture@gmail.com>
2022-04-13 21:20:36 +0300

    Now return ProjectColumn by column index and project id

--- project-server/src/main/java/com/alex/futurity/projectserver/service/ColumnService.java
+++ project-server/src/main/java/com/alex/futurity/projectserver/service/ColumnService.java
@@ -5,7 +5,9 @@ import com.alex.futurity.projectserver.entity.ProjectColumn;
 public interface ColumnService {
     void saveColumn(ProjectColumn column);
     ProjectColumn findColumnById(long id);
+    ProjectColumn findColumnByProjectColumnIndexAndProjectId(long columnIndex, long projectId);
     void deleteColumn(ProjectColumn column);
     void shiftColumnsIndex(int index, long projectId);
     void changeColumnIndex(int from, int to, long projectId);
+    ProjectColumn findColumn(long columnId, long projectId, long userId);
 }
