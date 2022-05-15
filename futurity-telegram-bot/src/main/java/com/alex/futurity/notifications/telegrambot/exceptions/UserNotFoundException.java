9406d1f9a41ec407aba59caf7068e6763190183e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-05-15 13:31:57 +0300

    Exception that occur when user not founded

---
+++ futurity-telegram-bot/src/main/java/com/alex/futurity/notifications/telegrambot/exceptions/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.alex.futurity.notifications.telegrambot.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
