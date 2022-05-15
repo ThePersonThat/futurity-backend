bc6717146592589a9a657053f6aa316d004dda21
Odeyalo <theprogrammerinfuture@gmail.com>
2022-05-15 22:26:30 +0300

    Now delegate command processing to CommandHandler

--- futurity-telegram-bot/src/main/java/com/alex/futurity/notifications/telegrambot/service/bot/commands/DisableNotificationCommand.java
+++ futurity-telegram-bot/src/main/java/com/alex/futurity/notifications/telegrambot/service/bot/commands/DisableNotificationCommand.java
@@ -1,34 +1,23 @@
 package com.alex.futurity.notifications.telegrambot.service.bot.commands;
 
-import com.alex.futurity.notifications.telegrambot.entity.TelegramUser;
-import com.alex.futurity.notifications.telegrambot.exceptions.UserNotFoundException;
-import com.alex.futurity.notifications.telegrambot.repository.TelegramUserRepository;
-import com.alex.futurity.notifications.telegrambot.service.bot.send.MessageSender;
-import com.alex.futurity.notifications.telegrambot.service.util.BotUtils;
+import com.alex.futurity.notifications.telegrambot.service.bot.commands.handler.CommandHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 public class DisableNotificationCommand implements Command {
-    private final TelegramUserRepository telegramUserRepository;
-    private final MessageSender messageSender;
+    private final CommandHandler commandHandler;
 
     @Autowired
-    public DisableNotificationCommand(TelegramUserRepository telegramUserRepository, MessageSender messageSender) {
-        this.telegramUserRepository = telegramUserRepository;
-        this.messageSender = messageSender;
+    public DisableNotificationCommand(@Qualifier("disableNotificationCommandHandler") CommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
     }
 
     @Override
     public void execute(Update update) throws Exception {
-        String chatId = BotUtils.getChatId(update);
-        TelegramUser telegramUser = this.telegramUserRepository.findTelegramUserByTelegramChatId(chatId);
-        if (telegramUser == null)
-            throw new UserNotFoundException("We can't find user with this chat id: " + chatId);
-        telegramUser.setEnabledNotifications(false);
-        this.telegramUserRepository.save(telegramUser);
-        this.messageSender.sendMessage("We are successful remove you from mailing list!", chatId);
+        this.commandHandler.handleCommand(update);
     }
 
     @Override
