4eefcc68737f059d00562e0418f84aff91b58492
Odeyalo <theprogrammerinfuture@gmail.com>
2022-05-18 21:01:37 +0300

    Return user id and nickname

--- user-server/src/main/java/com/alex/futurity/userserver/controller/UserController.java
+++ user-server/src/main/java/com/alex/futurity/userserver/controller/UserController.java
@@ -1,16 +1,23 @@
 package com.alex.futurity.userserver.controller;
 
+import com.alex.futurity.userserver.entity.User;
+import com.alex.futurity.userserver.repo.UserRepository;
 import com.alex.futurity.userserver.service.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @AllArgsConstructor
+@Log4j2
 public class UserController {
     private final UserService service;
 
@@ -19,5 +26,14 @@ public class UserController {
     })
     public ResponseEntity<Resource> getAvatar(@PathVariable long id) {
         return ResponseEntity.ok(service.findUserAvatar(id));
+    }
+
+    @GetMapping(value = "/{id}/id")
+    public ResponseEntity<?> getId(@PathVariable long id) {
+        HashMap<String, Object> answer = new HashMap<>();
+        answer.put("userId", id);
+        User user = service.findById(id);
+        answer.put("username", user.getNickname());
+        return new ResponseEntity<>(answer, HttpStatus.OK);
     }
 }
